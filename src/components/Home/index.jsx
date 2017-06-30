b8cd793b2bc7ef757cee956c443a108a29876a89
Pierre Nel <pierre.nel@gmail.com>
2017-06-30 09:54:22 +0100

    better home vertical position + skyline centering

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -54,53 +54,55 @@ class Home extends Component {
                 </div>
             </div>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-5 blurb">
+            <div className="container main">
+                <div className="tc">
+                    <div className="row">
+                        <div className="col-md-5 blurb">
 
-                        <h3 className="push-down">Life insurance<br /><span className="green">Simply</span>-fied</h3>
-                        <p className="intro">In 10 minutes, you could get instant cover and protect your family or employees. Insurance shouldn't be difficult, read more about our success <a href="#">#stories</a>.</p>
-                        <p className="intro">Life and disability insurance provides for your family when you can't. How much your family will need is different for every person and you should not spend more than you can afford.</p>
+                            <h3 className="push-down">Life insurance<br /><span className="green">Simply</span>-fied</h3>
+                            <p className="intro">In 10 minutes, you could get instant cover and protect your family or employees. Insurance shouldn't be difficult, read more about our success <a href="#">#stories</a>.</p>
+                            <p className="intro">Life and disability insurance provides for your family when you can't. How much your family will need is different for every person and you should not spend more than you can afford.</p>
 
-                        <Button {...buttonQuote} />
-                        <Button {...buttonCall} />
+                            <Button {...buttonQuote} />
+                            <Button {...buttonCall} />
 
-                    </div>
-                    <div className="col-md-6 col-md-push-1 push-down quotes">
-                        <div className="container-fluid why-simply">
-                            <div className="row">
-                                <Equalizer>
-                                <div className="col-xs-6 col-md-4 why-1">
-                                    <img src={svg_handshake} alt="Trusted insurer" />
-                                    <span>Trusted insurer</span>
-                                    <p>Policies are underwritten by Old Mutual Alternative Risk Transfer Limited.</p>
+                        </div>
+                        <div className="col-md-6 col-md-push-1 push-down quotes">
+                            <div className="container-fluid why-simply">
+                                <div className="row">
+                                    <Equalizer>
+                                    <div className="col-xs-6 col-md-4 why-1">
+                                        <img src={svg_handshake} alt="Trusted insurer" />
+                                        <span>Trusted insurer</span>
+                                        <p>Policies are underwritten by Old Mutual Alternative Risk Transfer Limited.</p>
+                                    </div>
+                                    <div className="col-xs-6 col-md-4 why-2">
+                                        <img src={svg_abc} alt="Easy to understand" />
+                                        <span>Easy to understand</span>
+                                        <p>Just 8 questions online, and policy documents you can understand.</p>
+                                    </div>
+                                    <div className="col-xs-6 col-md-4 why-3">
+                                        <img src={svg_3in1} alt="3 in 1 combos" />
+                                        <span>3 in 1 combos</span>
+                                        <p><a href="/buy">Buy a combo</a> of life, disability and funeral policies starting from only R55/month.</p>
+                                    </div>
+                                    <div className="col-xs-6 col-md-4 why-4">
+                                        <img src={svg_10min} alt="10 minute signup" />
+                                        <span>10 minute sign up</span>
+                                        <p>No paperwork. No blood tests. Instant cover. We can even <a href="#">call you</a> back.</p>
+                                    </div>
+                                    <div className="col-xs-6 col-md-4 why-5">
+                                        <img src={svg_greatvalue} alt="Great value" />
+                                        <span>Great value</span>
+                                        <p><a href="/quote">Get a quote</a> – you could save up to 50% on your insurance.</p>
+                                    </div>
+                                    <div className="col-xs-6 col-md-4 why-6">
+                                        <img src={svg_simplymoney} alt="Simply money" />
+                                        <span>Simply money</span>
+                                        <p>24 hour <a href="/claims">pay out</a> on funeral claims. R3,000 cash benefit. Optional cash back – 50%.</p>
+                                    </div>
+                                    </Equalizer>
                                 </div>
-                                <div className="col-xs-6 col-md-4 why-2">
-                                    <img src={svg_abc} alt="Easy to understand" />
-                                    <span>Easy to understand</span>
-                                    <p>Just 8 questions online, and policy documents you can understand.</p>
-                                </div>
-                                <div className="col-xs-6 col-md-4 why-3">
-                                    <img src={svg_3in1} alt="3 in 1 combos" />
-                                    <span>3 in 1 combos</span>
-                                    <p><a href="/buy">Buy a combo</a> of life, disability and funeral policies starting from only R55/month.</p>
-                                </div>
-                                <div className="col-xs-6 col-md-4 why-4">
-                                    <img src={svg_10min} alt="10 minute signup" />
-                                    <span>10 minute sign up</span>
-                                    <p>No paperwork. No blood tests. Instant cover. We can even <a href="#">call you</a> back.</p>
-                                </div>
-                                <div className="col-xs-6 col-md-4 why-5">
-                                    <img src={svg_greatvalue} alt="Great value" />
-                                    <span>Great value</span>
-                                    <p><a href="/quote">Get a quote</a> – you could save up to 50% on your insurance.</p>
-                                </div>
-                                <div className="col-xs-6 col-md-4 why-6">
-                                    <img src={svg_simplymoney} alt="Simply money" />
-                                    <span>Simply money</span>
-                                    <p>24 hour <a href="/claims">pay out</a> on funeral claims. R3,000 cash benefit. Optional cash back – 50%.</p>
-                                </div>
-                                </Equalizer>
                             </div>
                         </div>
                     </div>
